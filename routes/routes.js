aea21704ec763f6124771585ecf48ee44f963ef9
Joshua Flack <joshu.alexander@gmail.com>
2017-07-12 22:28:56 -0400

    all methods work via postman, need to fix stat update to only do one per day.

--- routes/routes.js
+++ routes/routes.js
@@ -31,22 +31,28 @@ router.post('/api/activities', (req, res) => {
 router.get('/api/activities/:id', (req, res) => {
   trakdstats.findById({
     _id: req.params.id
-  }, (err, foundStats) => {
+  }, (err, foundAct) => {
     if (err) return res.render('error', {
         error: "Could not find activity",
         title: "error"
       }),
-      console.log(foundStats);
+      console.log(foundAct);
 
-    res.send(foundStats);
+    res.render('act', {
+      act: foundAct
+    });
 
   })
 });
 
 //Update an activity, changing attributes
 router.put('/api/activities/:id', function(req, res) {
-  const act = {activity: req.body.putBox};
-  trakdstats.update({_id: req.params.id}, act, function(err, change) {
+  const act = {
+    activity: req.body.putBox
+  };
+  trakdstats.update({
+    _id: req.params.id
+  }, act, function(err, change) {
     console.log(change);
     if (err) {
       res.send(err);
@@ -56,26 +62,53 @@ router.put('/api/activities/:id', function(req, res) {
 });
 //Delete a single activity
 router.delete('/api/activities/:id', (req, res) => {
-  trakdstats.remove({ _id: req.params.id }, function(err) {
-      if (!err) {
-              res.send = 'Activity deleted.';
-              res.redirect('/api/activities')
-      }
-      else {
-              res.send = 'error';
-              res.redirect('/api/activities');
-      }
+  trakdstats.remove({
+    _id: req.params.id
+  }, function(err) {
+    if (!err) {
+      res.send = 'Activity deleted.';
+      res.redirect('/api/activities')
+    } else {
+      res.send = 'error';
+      res.redirect('/api/activities');
+    }
   });
 });
 
 //Add tracked data for the day, should include date and override existing data
 router.post('/api/activities/:id/stats', (req, res) => {
+  trakdstats.update({
+    _id: req.params.id
+  }, {
+    $push: {
+      activityArray: {
+        date: new Date(),
+        count: req.body.trackBox
+      }
+    }
+  }, function(err, newStat) {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/api/activities');
 
+  });
 });
 
 //Revove tracked data for a day
-router.delete('/api/stats/:id', (req, res) => {
-
+router.delete('/api/activities/:id/stats/:tId', (req, res) => {
+  trakdstats.update({
+    _id: req.params.id
+  }, { $pull: {"activityArray": {_id: req.params.tId}}},
+  function(err) {
+    if (!err) {
+      res.send = 'Activity deleted.';
+      res.redirect('/api/activities')
+    } else {
+      res.send = 'error';
+      res.redirect('/api/activities');
+    }
+  });
 });
 
 module.exports = router;
